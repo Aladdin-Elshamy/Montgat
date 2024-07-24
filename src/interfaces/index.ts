d8bdd59a14363e61f1ef54bf7f86bf8f2e02f677
Aladdin-Elshamy <122921299+Aladdin-Elshamy@users.noreply.github.com>
2024-07-24 18:32:27 +0300

    establish interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,25 @@
+export interface IProduct {
+    id?: string | undefined
+    title: string,
+    description: string,
+    price: string,
+    imageURL: string,
+    category: {
+      name: string
+      imageURL: string,
+    },
+    colors: string[],
+}
+
+export interface IFormData {
+    id: string,
+    label: string,
+    name: string,
+    type: string,
+}
+
+export interface ICategory {
+    id: string,
+    name: string,
+    imageURL: string,
+}
