d50954c7806c1ce438c3c3f88e123cff001f4989
Aladdin-Elshamy <122921299+Aladdin-Elshamy@users.noreply.github.com>
2024-07-27 18:36:48 +0300

    Add rest operator to button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from "react"
-interface IProps {
+import { ReactNode, ButtonHTMLAttributes } from "react"
+interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode
     className:string
     width?: "w-full" | "w-fit"
 }
-export default function Button({children,className,width="w-full"}:IProps) {
+export default function Button({children,className,width="w-full",...rest}:IProps) {
     return (
-        <button className={`${className} ${width}`}>{children}</button>
+        <button className={`${className} ${width}`} {...rest}>{children}</button>
     )
 }
