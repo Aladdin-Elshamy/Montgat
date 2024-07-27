8399e9f2c7de2b98b479b1bbc4edc8076136c9bb
Aladdin-Elshamy <122921299+Aladdin-Elshamy@users.noreply.github.com>
2024-07-27 19:01:44 +0300

    Visualize products in responsive container

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './index.css'
 import { productsList } from './data'
 import { useState } from 'react'
 import ProductCard from './components/ProductCard'
+import Button from './components/ui/Button'
 console.log(productsList)
 function App() {
   /* --------------------------------- States --------------------------------- */
@@ -11,8 +12,12 @@ function App() {
     return products.map(product => <ProductCard key={product.id} product={product} />)
   }
   return (
-    <main>
-      <div>
+    <main className='container my-10'>
+      <div className='flex justify-between mb-4'>
+        <h1 className='text-3xl font-bold text-indigo-500'>Latest Products</h1>
+        <Button className='bg-indigo-800 px-5 py-4 text-md' width='w-fit'>Add Product</Button>
+      </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4'>
         {renderProducts()}
       </div>
     </main>
